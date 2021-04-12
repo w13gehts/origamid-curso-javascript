7f68dfaef6854038c3bc0b85182e1951d8ba8eba
Juliana <juliana.salafia@gmail.com>
2021-04-12 17:39:31 -0300

    atribuicao e ternario

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 20;
+x += y; // x = x + y = (15)
+x -= y; // x = x - y = (-5)
+x *= y; // x = x * y = (50)
+x /= y; // x = x / y = (0.5)
+x %= y; // x = x % y = (0)
+x **= y; // x = x ** y = (9765625)
+
+//ternario
+var idade = 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,12 @@
+//some 500 ao valor de scroll abaixo atribuindo o novo valor a scroll 
+var scroll = 1000;
+scroll += 500;
+console.log(scroll);
+
+//atribua true par aa variavel darCredito, caso o cliente possua carro e casa e false caso contrario
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = (possuiCarro && possuiCasa);
+console.log(darCredito);
