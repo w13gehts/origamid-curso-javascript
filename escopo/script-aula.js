e712a69e7ab999d844967d298c0cd053fc45f072
Juliana <juliana.salafia@gmail.com>
2021-04-13 17:28:41 -0300

    escopo

---
+++ escopo/script-aula.js
@@ -0,0 +1,40 @@
+var carro = 'Fusca';
+
+function mostrarCarro() {  
+  console.log(carro);
+}
+
+mostrarCarro();
+console.log(carro);
+
+if (true) {
+  let mes = 'Dezembro';
+  console.log(mes);
+}
+
+{
+var carro2 = 'Ford Ká';
+var ano = 2018;
+}
+
+console.log(carro2);
+console.log(ano);
+
+var i = 50;
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Número ${i}`);
+}
+
+console.log(i * 10);
+
+const semana = "sexta";
+console.log(semana);
+
+const data = {
+  ano: 2019,
+  mes: 'Dezembro',
+}
+
+data.ano = 2019;
+data.dia = 25;
